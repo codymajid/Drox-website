99cf8ed82f36ea893aa0c9b7f05b7b127b3e563e
Majid Aziz <majidaziz7ctech@gmail.com>
2023-05-18 19:37:35 +0500

    hero fixed

--- Components/hero/Hero.jsx
+++ Components/hero/Hero.jsx
@@ -1,45 +1,35 @@
 import React from 'react'
 import styles from './hero.module.scss';
-import leftHeroImg from '../../images/left-hero.png'
-import heroMainImg from '../../images/hero-main.png'
-import rightImage from '../../images/right-hero.png'
 import Image from 'next/image';
-
+import LeftImage from '../../images/left-hero.png'
+import MainImage from '../../images/hero-main.png'
+import RightImage from '../../images/right-hero.png'
 
 const Hero = () => {
     return (
-        <>
-            <div className={styles.hero} id='home'>
-                <div className={styles.wrapper}>
-                    <div className={styles.shadowTopLeft}></div>
-                    <div className={styles.shadowRightLeft}></div>
-                    <div className={styles.top}>
-                        <h1> At Centricord We Make
-                            <span>Your Ideas Work.</span>
-                        </h1>
-                        <div className={styles.heroPara}>
-                            <span> Centicord is a central hub for those who have an idea in their mind,and require </span>
-                            <span> digital solutions to get them and going. We proivde a solid foundation </span>
-                        </div>
-                        <button className={styles.heroQuote}> Get a Quote </button>
-                    </div>
-                    <div className={styles.bottom}>
-                        <div className={styles.wrapperImages}>
-                            <div className={styles.leftImg}>
-                                <Image src={leftHeroImg} alt="image" />
-                            </div>
-                            <div className={styles.mainImg}>
-                                <Image src={heroMainImg} alt="image" />
-                            </div>
-                            <div className={styles.rightImg}>
-                                <Image src={rightImage} alt="image" />
-                            </div>
-                        </div>
-                        <div className={styles.wrapperBottom}></div>
-                    </div>
+        <div className={styles.hero}>
+            <div className={styles.leftHeroShadow}></div>
+            <div className={styles.rightHeroShadow}></div>
+            <div className={styles.top}>
+                <div className={styles.head}>
+                    <h1> At Centricord We Make  </h1>
+                    <h1> Your Ideas Work. </h1>
+                </div>
+                <div className={styles.paragraphHero}>
+                    <span> Centicord is a central hub for those who have an idea in their mind,and require   </span>
+                    <span>digital solutions to get them and going. We proivde a solid foundation </span>
+                </div>
+                <button> Get a Quote</button>
+            </div>
+            <div className={styles.bottom}>
+                <div className={styles.wrapperImages}>
+                    <Image src={LeftImage} alt='Image' className={styles.leftImage} />
+                    <Image src={MainImage} alt='Image' className={styles.mainImage} />
+                    <Image src={RightImage} alt='Image' className={styles.rightImage} />
                 </div>
             </div>
-        </>
+            <div className={styles.bottomWrapper}></div>
+        </div>
     )
 }
 
--- Components/services/Service.jsx
+++ Components/services/Service.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './service.module.scss';
 import { PanoramaFishEye } from '@mui/icons-material';
-import  {Type1, Type2, Type3, Type4, Type1Title,Type2Title, Type3Title, Type4Title, Type1Para, Type2Para,Type3Para,Type4Para, style1, style2} from '../../services'
+import { Type1, Type2, Type3, Type4, Type1Title, Type2Title, Type3Title, Type4Title, Type1Para, Type2Para, Type3Para, Type4Para, style1, style2 } from '../../services'
 import ServiceComp from '../servicesComponent/ServiceComp'
 import Image1 from '../../images/image1.png'
 import Image2 from '../../images/image2.png'
@@ -17,10 +17,12 @@ const Service = () => {
                     <h1> What Service We Offer </h1>
                     <p className={styles.paragraph}>Let’s talk about our services which can add value to your company and your ideas</p>
                 </div>
-                <ServiceComp data = {Type1} title = {Type1Title} text = {Type1Para} image = {Image1}  />
-                <ServiceComp data = {Type2} title = {Type2Title} text = {Type2Para} image = {Image2} />
-                <ServiceComp data = {Type3} title = {Type3Title} text = {Type3Para} image = {Image3} style={style1} />
-                <ServiceComp data = {Type4} title = {Type4Title} text = {Type4Para} image = {Image4} style={style2} />
+                <div className={styles.serviceBottom}>
+                    <ServiceComp data={Type1} title={Type1Title} text={Type1Para} image={Image1} />
+                    {/* <ServiceComp data = {Type2} title = {Type2Title} text = {Type2Para} image = {Image2} />
+                    <ServiceComp data = {Type3} title = {Type3Title} text = {Type3Para} image = {Image3} style={style1} />
+                    <ServiceComp data = {Type4} title = {Type4Title} text = {Type4Para} image = {Image4} style={style2} /> */}
+                </div>
             </div>
         </div>
     )
